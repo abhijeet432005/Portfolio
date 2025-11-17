f62fdabd0944e7eaff5500091cb15d855bc36885
Abhijeet kumar <abhijeetkumar@Abhijeets-MacBook-Air.local>
2025-11-17 19:47:12 +0530

    imporve

--- src/pages/ContactSummary.jsx
+++ src/pages/ContactSummary.jsx
@@ -8,28 +8,26 @@ const ContactSummary = () => {
     const items = ['Innovation', 'Precision', "Collaboration", "Excellence"]
     const items2 = ['contact', 'contact', 'contact', 'contact', 'contact', 'contact','contact','contact']
     const containerRef = useRef(null);
-    gsap.registerPlugin(ScrollTrigger)
+    // gsap.registerPlugin(ScrollTrigger)
 
-    useGSAP(() => {
-        gsap.to(containerRef.current, {
-            scrollTrigger: {
-                trigger: containerRef.current,
-                start: 'center center',
-                end: '+=800 center',
-                scrub: true,
-                pin: true,
-                pinSpacing: true
-            }
-        })
+    // useGSAP(() => {
+    //     gsap.to(containerRef.current, {
+    //         scrollTrigger: {
+    //             trigger: containerRef.current,
+    //             start: 'center center',
+    //             end: '+=800 center',
+    //             scrub: true,
+    //             pin: true,
+    //             pinSpacing: true
+    //         }
+    //     })
 
-        setTimeout(() => ScrollTrigger.refresh(), 200);
-    })
+    // })
 
 
   return (
     <>
-    <section className='h-16' />
-    <section ref={containerRef} className='min-h-screen flex flex-col items-center justify-between gap-12'>
+    <section ref={containerRef} className='min-h-screen flex flex-col items-center justify-between mt-18 gap-12'>
 
     <Marquee items={items} />
         <div className='overflow-hidden font-light text-center contact-text-responsive'>
