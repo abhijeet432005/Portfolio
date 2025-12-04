6ed60243c406c5f42798f969a4c418e01b1a81a6
Abhijeet kumar <abhijeetkumar@Abhijeets-MacBook-Air.local>
2025-12-04 18:05:12 +0530

    improvement

--- src/pages/ContactSummary.jsx
+++ src/pages/ContactSummary.jsx
@@ -5,40 +5,41 @@ import Marquee from '../components/contact/Marquee'
 import { ScrollTrigger } from 'gsap/all'
 
 const ContactSummary = () => {
-    const items = ['Innovation', 'Precision', "Collaboration", "Excellence"]
-    const items2 = ['contact', 'contact', 'contact', 'contact', 'contact', 'contact','contact','contact']
-    const containerRef = useRef(null);
-    // gsap.registerPlugin(ScrollTrigger)
+  const items = ['Innovation', 'Precision', "Collaboration", "Excellence"]
+  const items2 = ['contact', 'contact', 'contact', 'contact', 'contact', 'contact', 'contact', 'contact']
+  const containerRef = useRef(null);
+  
+  // gsap.registerPlugin(ScrollTrigger)
 
-    // useGSAP(() => {
-    //     gsap.to(containerRef.current, {
-    //         scrollTrigger: {
-    //             trigger: containerRef.current,
-    //             start: 'center center',
-    //             end: '+=800 center',
-    //             scrub: true,
-    //             pin: true,
-    //             pinSpacing: true
-    //         }
-    //     })
+  // useGSAP(() => {
+  //     gsap.to(containerRef.current, {
+  //         scrollTrigger: {
+  //             trigger: containerRef.current,
+  //             start: 'center center',
+  //             end: '+=800 center',
+  //             scrub: true,
+  //             pin: true,
+  //             pinSpacing: true
+  //         }
+  //     })
 
-    // })
+  // })
 
 
   return (
     <>
-    <section ref={containerRef} className='min-h-screen flex flex-col items-center justify-between mt-18 gap-12'>
+      <section ref={containerRef} className='min-h-screen flex flex-col items-center justify-between mt-30 gap-12'>
 
-    <Marquee items={items} />
+        <Marquee items={items} />
         <div className='overflow-hidden font-light text-center contact-text-responsive'>
-            <p>" Let's build a <br />
-                <span className='font-normal'>memorable</span> & {' '}
-                <span className='italic'>inspiring</span> <br />
-                website <span className='text-gold'>together</span> "
-            </p>
+          <p>" Let's build a <br />
+            <span className='font-normal'>memorable</span> & {' '}
+            <span className='italic'>inspiring</span> <br />
+            website <span className='text-gold'>together</span> "
+          </p>
         </div>
-    <Marquee items={items2} className='border-y-2 bg-transparent'/>
-    </section>
+        <Marquee items={items2} className='border-y-2 bg-transparent' />
+      </section>
     </>
   )
 }
